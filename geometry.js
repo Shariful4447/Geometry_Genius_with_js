f44bce7312692c66d00dfd454129ef06f7875283
Shariful4447 <shariful.islam4447@gmail.com>
2023-08-21 01:38:13 +0200

    add javascript to get value and area calculation done

--- geometry.js
+++ geometry.js
@@ -3,12 +3,19 @@ function calculateTriangleArea(){
     const baseField = document.getElementById('triangle-base');
     const baseValueText = baseField.value;
     const base = parseFloat(baseValueText);
+    console.log(base);
 
     // get triangle height
     const heightField = document.getElementById('triangle-height');
     const heightValueText = heightField.value;
     const height = parseFloat(heightValueText);
-
-    console.log(base);
     console.log(height);
+
+    // calculate area of triangle
+    const triangleArea =0.5 * base *height;
+    console.log(triangleArea);
+
+    // Show triangle area
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = triangleArea;
 }
